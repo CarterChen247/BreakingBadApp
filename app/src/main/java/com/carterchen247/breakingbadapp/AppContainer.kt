c377ffe8a4caffaf7492d34052b827b6083c5171
CarterChen247 <carterchen247@gmail.com>
2020-08-08 12:11:56 +0800

    feat: get data from internet done

--- app/src/main/java/com/carterchen247/breakingbadapp/AppContainer.kt
+++ app/src/main/java/com/carterchen247/breakingbadapp/AppContainer.kt
@@ -1,10 +1,12 @@
 package com.carterchen247.breakingbadapp
 
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import timber.log.Timber
 
 class AppContainer {
     private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
         .baseUrl("https://www.breakingbadapi.com/api/")
         .build()
     private val apiService = retrofit.create(ApiService::class.java)
--- app/src/main/java/com/carterchen247/breakingbadapp/MainActivity.kt
+++ app/src/main/java/com/carterchen247/breakingbadapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.carterchen247.breakingbadapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnGetCharacterInfo.setOnClickListener {
+            (application as App).appContainer.remoteDataSource.apiService.getCharacterInfo(1).enqueue(object : Callback<String> {
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    Timber.d("get character info onFailure")
+                }
+
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    Timber.d("get character info onResponse")
+                }
+            })
             Timber.d("get character info")
         }
     }
