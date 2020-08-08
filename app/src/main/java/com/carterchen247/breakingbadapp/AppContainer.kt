76244861ef21b2926860199bebac7c4757f2d07f
CarterChen247 <carterchen247@gmail.com>
2020-08-08 12:19:23 +0800

    refactor: change component init in AppContainer.kt to lazy init

--- app/src/main/java/com/carterchen247/breakingbadapp/AppContainer.kt
+++ app/src/main/java/com/carterchen247/breakingbadapp/AppContainer.kt
@@ -5,12 +5,22 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 import timber.log.Timber
 
 class AppContainer {
-    private val retrofit = Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .baseUrl("https://www.breakingbadapi.com/api/")
-        .build()
-    private val apiService = retrofit.create(ApiService::class.java)
-    val remoteDataSource = RemoteDataSource(apiService)
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .baseUrl("https://www.breakingbadapi.com/api/")
+            .build()
+    }
+
+    private val apiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+
+    val remoteDataSource by lazy {
+        RemoteDataSource(apiService)
+    }
+
     init {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
