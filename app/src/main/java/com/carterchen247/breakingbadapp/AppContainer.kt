d0e6782b518aa965fbfad8b16e29a3b0ba71517d
CarterChen247 <carterchen247@gmail.com>
2020-08-08 12:21:11 +0800

    feat: add HttpLoggingInterceptor

--- app/src/main/java/com/carterchen247/breakingbadapp/AppContainer.kt
+++ app/src/main/java/com/carterchen247/breakingbadapp/AppContainer.kt
@@ -1,15 +1,30 @@
 package com.carterchen247.breakingbadapp
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import timber.log.Timber
 
 class AppContainer {
 
+    private val httpLoggingInterceptor by lazy {
+        HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    private val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
     private val retrofit by lazy {
         Retrofit.Builder()
             .addConverterFactory(ScalarsConverterFactory.create())
             .baseUrl("https://www.breakingbadapi.com/api/")
+            .client(okHttpClient)
             .build()
     }
 
